4a8c15151dfd04aefc2a2fe2c8a67c83ced08f91
Darius Kruythoff <dkruythoff@gmail.com>
2020-05-19 10:48:35 +0200

    Connected external core lib

--- src/components/Connect4.vue
+++ src/components/Connect4.vue
@@ -1,13 +1,25 @@
 <template>
-  <div :class="{'game':true,'game--over':gameIsWon}">
+  <div
+    :class="{
+      'game': true,
+      'game--over': gameState.gameover
+    }">
     <div
-      v-if="board"
-      :class="{'board':true,'board--playable':!gameIsWon,'board--turn-1':turn===1,'board--turn-2':turn===2}"
+      :class="{
+        'board':true,
+        'board--playable': !gameState.gameover,
+        'board--turn-1': gameState.turn === 1,
+        'board--turn-2': gameState.turn === 2
+        }"
       >
       <div
-        v-for="(column, columnIndex) in board"
+        v-for="(column, columnIndex) in gameState.board"
         :key="`board-column-${columnIndex}`"
-        :class="{'column':true,'column--full':freeIndexPerCol[columnIndex]===-1,'column--highlighted':highlightedColumn===columnIndex}"
+        :class="{
+          'column': true,
+          'column--full': !gameState.columnsPlayable,
+          'column--highlighted': highlightedColumn === columnIndex
+        }"
         @touchend="handleColumnTouchend(columnIndex)"
         @click="handleColumnClick(columnIndex)"
         >
@@ -15,30 +27,42 @@
             v-for="(cell, cellIndex) in column"
             :key="`board-column-${columnIndex}-cell-${cellIndex}`"
             :data-position="`${columnIndex}x${cellIndex}`"
-            :class="{'cell':true,'cell--winner':cell.winner}"
+            :class="{
+              'cell':true,
+              'cell--winner':cell.winner
+            }"
             >
-            <span :class="{'piece':true,'piece--1':cell.value===1,'piece--2':cell.value===2}">&nbsp;</span>
+            <span :class="{
+              'piece': true,
+              'piece--1': cell.value === 1,
+              'piece--2': cell.value === 2
+            }">&nbsp;</span>
           </div>
           <div
             class="cell cell--preview"
             >
-            <span :class="{'piece':true,'piece--1':turn===1,'piece--2':turn===2}">&nbsp;</span>
+            <span :class="{
+              'piece': true,
+              'piece--1': gameState.turn === 1,
+              'piece--2': gameState.turn === 2
+            }">&nbsp;</span>
           </div>
       </div>
     </div>
     <div class="won">
-      <p>{{ turn === 1 ? 'Red' : 'Yellow' }} wins!</p>
+      <p>{{ gameState.turn === 1 ? 'Red' : 'Yellow' }} wins!</p>
       <button class="startgame" @click="startgame">Play again</button>
     </div>
   </div>
 </template>
 
 <script>
-import { Connect4 } from '../lib/Connect4'
+import { play } from 'connect4-core'
 
 export default {
   data() {
     return {
+      gameState: null,
       game: null,
       board: null,
       turn: 1,
@@ -57,74 +81,47 @@ export default {
       default: 6
     }
   },
-  computed: {
-    freeIndexPerCol() {
-      return this.board.map(col => col.findIndex(cell => cell.value === null))
-    },
-    gameIsWon() {
-      return !!this.winner
-    }
-  },
   methods: {
     startgame() {
-      this.game = new Connect4(this.cols, this.rows)
-      this.updateFromGame()
+      const {cols,rows} = this
+      this.gameState = play({cols,rows})
     },
-    handleColumnTouchend(col) {
+    handleColumnTouchend(columnIndex) {
       this.touchEnabled = true
 
-      if (this.gameIsWon || this.freeIndexPerCol[col] === -1) {
+      if (!this.columnIsPlayable(columnIndex)) {
         return
       }
 
-      if (this.highlightedColumn !== col) {
-        this.highlightedColumn = col
+      if (this.highlightedColumn !== columnIndex) {
+        this.highlightedColumn = columnIndex
       } else {
-        this.makeMove(col)
         this.highlightedColumn = null
+        this.makeMove(columnIndex)
       }
 
     },
-    handleColumnClick(col) {
+    handleColumnClick(columnIndex) {
       if (this.touchEnabled) {
         this.touchEnabled = false
         return
       }
 
-      if (this.gameIsWon || this.freeIndexPerCol[col] === -1) return
-
-      if (this.touchEnabled) {
-        this.touchEnabled = false
-      } else {
-        this.makeMove(col)
+      if (!this.columnIsPlayable(columnIndex)) {
+        return
       }
+
+      this.makeMove(columnIndex)
     },
     makeMove(col) {
-      this.game.makeMove(col)
-      this.updateFromGame()
+      this.$set(this,'gameState',play(this.gameState, col))
     },
-    updateFromGame() {
-      this.turn = this.game.turn
-      this.winner = this.game.winner
-
-      const winningCoordinates = this.winner
-        ? this.winner.winningCoordinates.map(c => c.toString())
-        : undefined
-
-      this.board = [...this.game.board.map(
-        (col, x) => col.map(
-          (cell, y) => {
-            const value = cell
-            const winner = winningCoordinates
-              ? winningCoordinates.indexOf([x, y].toString()) !== -1
-              : false
-
-            return { value, winner }
-        })
-      )]
+    columnIsPlayable(columnIndex) {
+      const {gameState: {gameover, columnsPlayable }} = this
+      return !(gameover || !columnsPlayable[columnIndex])
     }
   },
-  mounted() {
+  created() {
     this.startgame()
   }
 }
